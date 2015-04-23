3ea4373ea22e763ff9b0d1eda364d249cfc50a33
Ross Gardiner <ross-public@gmx.com>
2015-04-23 14:52:07 +0100

    Initial PointCloud class for convex hull algorithm

---
+++ COMS10001-algorithms/PointCloud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace COMS10001_algorithms
+{
+    public class PointCloud
+    {
+        private ISet<Tuple<int, int>> _points;
+ 
+        public PointCloud(ISet<Tuple<int, int>> points)
+        {
+            this._points = points;
+        }
+
+        public List<Tuple<int, int>> ConvexHull()
+        {
+            return null;
+        } 
+    }
+}
---
+++ COMS10001-algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMS10001_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
